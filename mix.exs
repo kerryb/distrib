3d1dd79d9319e4fd86832f1c2b6964a00c4498a5
Kerry Buckley <kerryjbuckley@gmail.com>
2021-12-14 11:26:41 +0000

    Sort deps

--- mix.exs
+++ mix.exs
@@ -33,17 +33,17 @@ defmodule Distrib.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
+      {:floki, ">= 0.30.0", only: :test},
+      {:jason, "~> 1.2"},
       {:phoenix, "~> 1.6.4"},
       {:phoenix_html, "~> 3.0"},
+      {:phoenix_live_dashboard, "~> 0.6"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.6"},
-      {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
+      {:plug_cowboy, "~> 2.5"},
       {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
-      {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:telemetry_poller, "~> 1.0"}
     ]
   end
 
