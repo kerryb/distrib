916e05e0aad7fd7caab2b653c20527f4120982b5
Kerry Buckley <kerryjbuckley@gmail.com>
2021-12-15 16:28:53 +0000

    Show which node queue is running on in liveview

--- lib/distrib/queue.ex
+++ lib/distrib/queue.ex
@@ -1,6 +1,9 @@
 defmodule Distrib.Queue do
   use GenServer
+
   require Logger
+
+  alias Phoenix.PubSub
 
   def child_spec(opts) do
     name = Keyword.get(opts, :name, __MODULE__)
@@ -26,8 +29,23 @@ defmodule Distrib.Queue do
   end
 
   def init(_args) do
+    PubSub.subscribe(Distrib.PubSub, "tasks")
+    report_queue_node()
     {:ok, nil}
   end
 
   def via_tuple(name), do: {:via, Horde.Registry, {Distrib.Registry, name}}
+
+  def handle_info(:ping_queue, state) do
+    report_queue_node()
+    {:noreply, state}
+  end
+
+  def handle_info(message, state) do
+    Logger.info("#{__MODULE__} ignoring message #{inspect(message)}")
+    {:noreply, state}
+  end
+  defp report_queue_node do
+    PubSub.broadcast!(Distrib.PubSub, "tasks", {:queue_running, node()})
+  end
 end
--- lib/distrib_web/live/index_live.ex
+++ lib/distrib_web/live/index_live.ex
@@ -1,10 +1,20 @@
 defmodule DistribWeb.IndexLive do
   use DistribWeb, :live_view
 
+  require Logger
+
   alias Phoenix.PubSub
 
   def render(assigns) do
     ~H"""
+    <p>
+      <%= if @queue_node do %>
+        Queue is running on <%= inspect @queue_node %>
+      <% else %>
+        Queue is not running
+      <% end %>
+    </p>
+
     <table>
       <tr><th>Task number</th><th>Started</th><th>Finished</th></tr>
       <%= for task <- @tasks do %>
@@ -21,9 +31,14 @@ defmodule DistribWeb.IndexLive do
   def mount(_params, _session, socket) do
     if connected?(socket) do
       PubSub.subscribe(Distrib.PubSub, "tasks")
+      PubSub.broadcast!(Distrib.PubSub, "tasks", :ping_queue)
     end
 
-    {:ok, assign(socket, tasks: [])}
+    {:ok, assign(socket, queue_node: nil, tasks: [])}
+  end
+
+  def handle_info({:queue_running, node}, socket) do
+    {:noreply, assign(socket, queue_node: node)}
   end
 
   def handle_info({:task_started, number}, socket) do
@@ -31,5 +46,10 @@ defmodule DistribWeb.IndexLive do
      assign(socket,
        tasks: [%{number: number, started?: true, finished?: false} | socket.assigns.tasks]
      )}
+  end
+
+  def handle_info(message, socket) do
+    Logger.info("#{__MODULE__} ignoring message #{inspect(message)}")
+    {:noreply, socket}
   end
 end
